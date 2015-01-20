cebe30fec71e36a629ae01b08316ab77b9c671d4
Daniel Cousens <github@dcousens.com>
2015-01-20 16:28:43 +1100

    transactions: inline __get

--- src/transactions.js
+++ src/transactions.js
@@ -25,52 +25,6 @@ Transactions.prototype.__validateTxId = function(string) {
 //propagate: function(req, res) {
 //summary: function(req, res) {
 
-Transactions.prototype.__get = function(client, txId, callback) {
-  var queryInfo = sql.getInfo({ txId: txId })
-  var queryInputs = sql.getInputs({ txId: txId })
-  var queryOutputs = sql.getOutputs({ txId: txId })
-
-  async.parallel([
-    client.query.bind(client, queryInfo),
-    client.query.bind(client, queryInputs),
-    client.query.bind(client, queryOutputs)
-  ], function(err, results) {
-    if (err) return callback(err)
-
-    var info = results[0].rows[0]
-    var inputs = results[1].rows
-    var outputs = results[2].rows
-
-    if (!info) return callback(new Error(txId + ' not found'))
-
-    var tx = new bitcoinjs.Transaction()
-
-    tx.locktime = parseInt(info.tx_locktime)
-    tx.version = parseInt(info.tx_version)
-
-    inputs.forEach(function(txIn) {
-      var index = parseInt(txIn.prev_txout_pos)
-      var script = bitcoinjs.Script.fromHex(txIn.txin_scriptsig)
-      var sequence = parseInt(txIn.txin_sequence)
-      var txId = txIn.prev_tx_hash
-
-      tx.addInput(txId, index, sequence, script)
-    })
-
-    outputs.forEach(function(txOut) {
-      var script = bitcoinjs.Script.fromHex(txOut.txout_scriptpubkey)
-      tx.addOutput(script, parseInt(txOut.txout_value))
-    })
-
-    return callback(undefined, {
-      txId: txId,
-      txHex: tx.toHex(),
-      blockId: info.block_hash,
-      blockHeight: info.block_height
-    })
-  })
-}
-
 Transactions.prototype.get = function(req, res) {
   var txIds = req.body.txIds
   var self = this
@@ -87,7 +41,49 @@ Transactions.prototype.get = function(req, res) {
     if (err) return res.jsend.error(err.message)
 
     async.map(txIds, function(txId, callback) {
-      self.__get(client, txId, callback)
+      var queryInfo = sql.getInfo({ txId: txId })
+      var queryInputs = sql.getInputs({ txId: txId })
+      var queryOutputs = sql.getOutputs({ txId: txId })
+
+      async.parallel([
+        client.query.bind(client, queryInfo),
+        client.query.bind(client, queryInputs),
+        client.query.bind(client, queryOutputs)
+      ], function(err, results) {
+        if (err) return callback(err)
+
+        var info = results[0].rows[0]
+        var inputs = results[1].rows
+        var outputs = results[2].rows
+
+        if (!info) return callback(new Error(txId + ' not found'))
+
+        var tx = new bitcoinjs.Transaction()
+
+        tx.locktime = parseInt(info.tx_locktime)
+        tx.version = parseInt(info.tx_version)
+
+        inputs.forEach(function(txIn) {
+          var index = parseInt(txIn.prev_txout_pos)
+          var script = bitcoinjs.Script.fromHex(txIn.txin_scriptsig)
+          var sequence = parseInt(txIn.txin_sequence)
+          var txId = txIn.prev_tx_hash
+
+          tx.addInput(txId, index, sequence, script)
+        })
+
+        outputs.forEach(function(txOut) {
+          var script = bitcoinjs.Script.fromHex(txOut.txout_scriptpubkey)
+          tx.addOutput(script, parseInt(txOut.txout_value))
+        })
+
+        return callback(undefined, {
+          txId: txId,
+          txHex: tx.toHex(),
+          blockId: info.block_hash,
+          blockHeight: info.block_height
+        })
+      })
     }, function(err, results) {
       free()
 
