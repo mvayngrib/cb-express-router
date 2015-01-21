c9471051238684df8131ea67bb40730d90b431b7
Daniel Cousens <github@dcousens.com>
2015-01-21 18:14:58 +1100

    blocks: add impl. for latest

--- src/blocks.js
+++ src/blocks.js
@@ -3,7 +3,8 @@ var typeforce = require('typeforce')
 var utils = require('./utils')
 
 var sql = {
-  get: swig.compileFile('./src/sql/blocks.sql')
+  get: swig.compileFile('./src/sql/blocks.sql'),
+  latest: swig.compileFile('./src/sql/blocksLatest.sql')
 }
 
 function Blocks(connString, rpc) {
@@ -16,7 +17,25 @@ Blocks.prototype.get = function(req, res) {
 }
 
 Blocks.prototype.latest = function(req, res) {
-  return res.jsend.fail('TODO')
+  var query = sql.latest({ limit: 1 })
+
+  utils.runQuery(this.connString, query, [], function(err, results) {
+    if (err) return res.jsend.error(err.message)
+
+    var latest = results.pop()
+
+    return res.jsend.success({
+      blockId: latest.block_hash,
+      prevBlockId: latest.prev_block_hash,
+      merkleRootHash: latest.block_hashmerkleroot,
+      nonce: latest.block_nonce,
+      version: latest.block_version,
+      blockHeight: latest.block_height,
+      blockSize: latest.block_bits,
+      timestamp: latest.block_timestamp,
+      txCount: latest.block_tx_count
+    })
+  })
 }
 
 Blocks.prototype.propagate = function(req, res) {
