aadd0acc2f83f91640aa185b166ca4e35a6c36c7
Daniel Cousens <github@dcousens.com>
2015-01-21 18:55:11 +1100

    sql: fix AS statements attempting tolower w/o quotes

--- src/sql/blocksLatest.sql
+++ src/sql/blocksLatest.sql
@@ -1,13 +1,13 @@
 SELECT 
-block_hash AS blockId,
-prev_block_hash AS prevBlockId,
-block_hashmerkleroot AS merkleRootHash,
-block_nonce AS nonce,
-block_version AS version,
-block_height AS blockHeight,
-block_bits AS blockSize,
-block_timestamp AS timestamp,
-block_tx_count AS txCount
+block_hash AS "blockId",
+prev_block_hash AS "prevBlockId",
+block_hashmerkleroot AS "merkleRootHash",
+block_nonce AS "nonce",
+block_version AS "version",
+block_height AS "blockHeight",
+block_bits AS "blockSize",
+block_timestamp AS "timestamp",
+block_tx_count AS "txCount"
 FROM block_view
 WHERE block_in_longest = true
 ORDER BY block_height DESC
