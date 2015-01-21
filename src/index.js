083a91fefa2c94ab619ccd5e3479073463111d3f
Daniel Cousens <github@dcousens.com>
2015-01-21 14:40:20 +1100

    index: add rpc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+var bitcoin = require('bitcoin')
 var express = require('express')
 var bodyParser = require('body-parser')
 var jsend = require('jsend')
@@ -15,18 +16,21 @@ function createApp(config) {
   // jsend
   app.use(jsend.middleware)
 
+  // rpc api
+  var rpc = new bitcoin.Client(config.rpc)
+
   // common blockchain api
   var addresses = new Addresses(config.postgres, config.network)
   app.post('/addresses/summary', addresses.summary.bind(addresses))
   app.post('/addresses/transactions', addresses.transactions.bind(addresses))
   app.post('/addresses/unspents', addresses.unspents.bind(addresses))
 
-//    var blocks = new Blocks(connString)
+//    var blocks = new Blocks(config.postgres, rpc)
 //  app.post('/blocks/get', blocks.get.bind(blocks))
 //  app.post('/blocks/latest', blocks.latest.bind(blocks))
 //  app.post('/blocks/propagate', blocks.propagate.bind(blocks))
 
-  var transactions = new Transactions(config.postgres)
+  var transactions = new Transactions(config.postgres, rpc)
   app.post('/transactions/get', transactions.get.bind(transactions))
 //  app.post('/transactions/latest', transactions.latest.bind(transactions))
 //  app.post('/transactions/propagate', transactions.propagate.bind(transactions))
