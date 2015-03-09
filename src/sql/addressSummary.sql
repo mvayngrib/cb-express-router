83bf9e37d6944e6c2b4c3bb55d73e7d2958f3155
Daniel Cousens <github@dcousens.com>
2015-03-09 17:14:00 +1100

    sql: LEFT JOIN should be a FULL JOIN

--- src/sql/addressSummary.sql
+++ src/sql/addressSummary.sql
@@ -5,7 +5,7 @@ COALESCE(confirmed.confirmed_balance, 0) + COALESCE(unconfirmed.unconfirmed_bala
 COALESCE(confirmed.confirmed_received_amount, 0) + COALESCE(unconfirmed.unconfirmed_received_amount, 0) AS "totalReceived", 
 COALESCE(confirmed.confirmed_tx_count, 0) + COALESCE(unconfirmed.unconfirmed_tx_count, 0) AS "txCount"
 FROM addr_summary AS confirmed
-LEFT JOIN unconfirmed_addr_summary AS unconfirmed
+FULL JOIN unconfirmed_addr_summary AS unconfirmed
 USING (addr_bs58)
 WHERE addr_bs58 = {{address|safe}}
 {% if loop.last !== true %}
