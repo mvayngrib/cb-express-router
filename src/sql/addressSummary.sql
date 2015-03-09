45de23aab233b2624c60a32bf1ecb81bac8e57fe
Daniel Cousens <github@dcousens.com>
2015-03-09 17:45:16 +1100

    sql: use helloblock-style query until changeover

--- src/sql/addressSummary.sql
+++ src/sql/addressSummary.sql
@@ -1,14 +1,24 @@
 {% for address in addresses %}
 SELECT
-addr_bs58 AS "address",
-COALESCE(confirmed.confirmed_balance, 0) + COALESCE(unconfirmed.unconfirmed_balance, 0) AS "balance",
-COALESCE(confirmed.confirmed_received_amount, 0) + COALESCE(unconfirmed.unconfirmed_received_amount, 0) AS "totalReceived", 
-COALESCE(confirmed.confirmed_tx_count, 0) + COALESCE(unconfirmed.unconfirmed_tx_count, 0) AS "txCount"
-FROM addr_summary AS confirmed
-FULL JOIN unconfirmed_addr_summary AS unconfirmed
+  COALESCE(confirmed.addr_bs58, unconfirmed.addr_bs58) AS "address",
+   COALESCE(confirmed.confirmed_tx_count, 0::bigint)::bigint + COALESCE(unconfirmed.unconfirmed_tx_count, 0::bigint)::bigint AS "balance",
+   COALESCE(confirmed.confirmed_balance, 0::bigint)::bigint + COALESCE(unconfirmed.unconfirmed_balance, 0::bigint)::bigint AS "totalReceived",
+   COALESCE(confirmed.confirmed_received_amount, 0::bigint)::bigint + COALESCE(unconfirmed.unconfirmed_received_amount, 0::bigint)::bigint AS "txCount"
+FROM (
+  SELECT
+    addr_summary.addr_bs58,
+    addr_summary.confirmed_tx_count,
+    addr_summary.confirmed_balance,
+    addr_summary.confirmed_received_amount
+  FROM addr_summary
+  WHERE addr_bs58 = {{address|safe}}
+) AS confirmed
+FULL JOIN (
+  SELECT * FROM unconfirmed_addr_summary
+  WHERE addr_bs58 = {{address|safe}}
+) As unconfirmed
 USING (addr_bs58)
-WHERE addr_bs58 = {{address|safe}}
-{% if loop.last !== true %}
-UNION ALL
-{% endif %}
+  {% if loop.last !== true %}
+    UNION ALL
+  {% endif %}
 {% endfor %}
