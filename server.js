5a1cf28e9da690d2d37398ffa550bac2ab04cc89
Daniel Cousens <github@dcousens.com>
2015-04-14 21:48:59 +1000

    use timeago module

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var cors = require('cors')
 var express = require('express')
 var morgan = require('morgan')
 var swig = require('swig')
+var timeago = require('timeago')
 
 var createRouter = require('./lib')
 
@@ -42,38 +43,13 @@ app.use(api)
 
 var startTime = new Date()
 
-function timeSince(date) {
-  var str = ""
-  var seconds = Math.floor((new Date() - date) / 1000)
-
-  var years = Math.floor(seconds / 31536000)
-  if (years) str += years + " Years "
-
-  var months = Math.floor(seconds / 2592000) % 12
-  if (months) str += months + " Months "
-
-  var days = Math.floor(seconds / 86400) % 365
-  if (days) str += days + " Days "
-
-  var hours = Math.floor(seconds / 3600) % 24
-  if (hours) str += hours + " Hours "
-
-  var minutes = Math.floor(seconds / 60) % 60
-  if (minutes) str += minutes + " Minutes "
-
-  seconds = seconds % 60
-  if (seconds) str += seconds + " Seconds"
-
-  return str
-}
-
 app.engine('html', swig.renderFile)
 app.set('view engine', 'html')
 app.set('views', __dirname + '/views')
 app.get('/', function(req, res) {
   res.render('index', {
     displayName: config.displayName,
-    uptime: timeSince(startTime),
+    uptime: timeago(+startTime),
     requests: api.requestCount
   })
 })
